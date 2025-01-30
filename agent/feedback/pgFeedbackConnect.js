a452675bf1c541f62a57c36a72b4a2df3cfc77a5
avanishmaurya <avanish8382066@gmail.com>
2025-01-30 13:12:05 +0530

    all pg connect modified

--- agent/feedback/pgFeedbackConnect.js
+++ agent/feedback/pgFeedbackConnect.js
@@ -3,15 +3,15 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const pool = new Pool({
-    user:  process.env.AGENT_PORTAL_USER,
+    user:  process.env.PORTAL_USER,
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
-    password: process.env.AGENT_PORTAL_PASSWORD,
-    database: process.env.PG_DATABASE_AGENT_PORTAL
+    password: process.env.PORTAL_PASSWORD,
+    database: process.env.PG_DATABASE_AGENTS
  })
 
  pool.on('error', (err) => {
-    console.error('An idle client has experienced an error in pgReferralConnect', err.stack)
+    console.error('An idle client has experienced an error in pgUserAuthConnect', err.stack)
  })
 
  module.exports = pool
